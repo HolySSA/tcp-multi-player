e074321a00b34dff6081d0c30e5e5ba4caa18dd2
SSA <158470963+shinseongan@users.noreply.github.com>
2024-10-22 22:29:24 +0900

    Feat: 이벤트 분리

---
+++ src/events/onError.js
@@ -0,0 +1,12 @@
+//import { removeUser } from '../session/user.session.js';
+//import CustomError from '../utils/error/customError.js';
+//import { ErrorCodes } from '../utils/error/errorCodes.js';
+//import { handleError } from '../utils/error/errorHandler.js';
+
+export const onError = (socket) => (err) => {
+  console.error('Socket error:', err);
+  //handleError(socket, new CustomError(ErrorCodes.SOCKET_ERROR, `소켓 오류: ${err.message}`));
+
+  // 세션에서 유저 삭제
+  //removeUser(socket);
+};
