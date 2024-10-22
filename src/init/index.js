2e41e383730f07c83d8dd2f4dc2c82ae0ade90de
SSA <158470963+shinseongan@users.noreply.github.com>
2024-10-22 22:29:49 +0900

    Feat: 초기 설정

---
+++ src/init/index.js
@@ -0,0 +1,14 @@
+//import { loadGameAssets } from './assets.js';
+
+const initServer = async () => {
+  /*
+  try {
+    await loadGameAssets();
+  } catch (err) {
+    console.error(err);
+    process.exit(1); // 에러 발생 시 게임 종료
+  }
+    */
+};
+
+export default initServer;
