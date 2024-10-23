0e10aa4ad486ce907bef91ac0f6ae45e3cf7d26d
SSA <158470963+shinseongan@users.noreply.github.com>
2024-10-23 16:53:09 +0900

    Feat: 패킷 변환 함수

---
+++ src/utils/parser/packetParser.js
@@ -0,0 +1,23 @@
+import { getProtoMessages } from '../../init/loadProtos.js';
+
+export const packetParser = (data) => {
+  const protoMessages = getProtoMessages();
+
+  // 공통 패킷 구조를 디코딩
+  const Packet = protoMessages.common.Packet;
+  let packet;
+  try {
+    packet = Packet.decode(data);
+  } catch (error) {
+    console.error(error);
+  }
+
+  const handlerId = packet.handlerId;
+  const userId = packet.userId;
+  const version = packet.version;
+  const payload = packet.payload;
+
+  console.log('version:', version);
+
+  return { handlerId, userId, payload };
+};
