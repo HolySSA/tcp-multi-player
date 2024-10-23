3135d0d00bba0acf7fc238bbe62a97397f2f6043
SSA <158470963+shinseongan@users.noreply.github.com>
2024-10-23 22:33:27 +0900

    Feat: db 세팅

--- src/init/index.js
+++ src/init/index.js
@@ -1,8 +1,11 @@
 import { loadProtos } from './loadProtos.js';
+import { dbAllConnections } from '../utils/db/dbconnection.js';
+import pools from '../db/database.js';
 
 const initServer = async () => {
   try {
     await loadProtos();
+    await dbAllConnections(pools);
   } catch (err) {
     console.error(err);
     process.exit(1); // 에러 발생 시 게임 종료
