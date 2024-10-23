3821b6cfa236d021aca8e187941db2dfdc0ace3d
SSA <158470963+shinseongan@users.noreply.github.com>
2024-10-23 16:53:40 +0900

    Fix: 프로토파일 로드 함수 추가

--- src/init/index.js
+++ src/init/index.js
@@ -1,14 +1,12 @@
-//import { loadGameAssets } from './assets.js';
+import { loadProtos } from './loadProtos.js';
 
 const initServer = async () => {
-  /*
   try {
-    await loadGameAssets();
+    await loadProtos();
   } catch (err) {
     console.error(err);
     process.exit(1); // 에러 발생 시 게임 종료
   }
-    */
 };
 
 export default initServer;
