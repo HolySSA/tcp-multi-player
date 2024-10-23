6a58cecdff02bf83b84a1763254ab362206e7ba8
SSA <158470963+shinseongan@users.noreply.github.com>
2024-10-23 16:57:31 +0900

    Feat: 프로토 패킷 구조

---
+++ src/protobuf/packetNames.js
@@ -0,0 +1,12 @@
+export const packetNames = {
+  initial: {
+    InitialPacket: 'initial.InitialPacket',
+  },
+  common: {
+    Packet: 'common.Packet',
+    Ping: 'common.Ping',
+  },
+  response: {
+    Response: 'response.Response',
+  },
+};
