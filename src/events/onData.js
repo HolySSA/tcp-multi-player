429a7c318802b757535264947b2bc85086d45ec8
SSA <158470963+shinseongan@users.noreply.github.com>
2024-10-23 20:37:40 +0900

    Fix: 핸들러, 에러 관리 추가

--- src/events/onData.js
+++ src/events/onData.js
@@ -1,6 +1,8 @@
 import { config } from '../config/config.js';
 import { packetParser } from '../utils/parser/packetParser.js';
 import { PACKET_TYPE } from '../constants/header.js';
+import { getHandlerById } from '../handlers/index.js';
+import { handleError } from '../utils/error/errorHandler.js';
 
 export const onData = (socket) => async (data) => {
   socket.buffer = Buffer.concat([socket.buffer, data]);
@@ -21,19 +23,26 @@ export const onData = (socket) => async (data) => {
       const packet = socket.buffer.slice(totalHeaderLength, length);
       socket.buffer = socket.buffer.slice(length);
 
-      console.log(`length: ${length}`);
-      console.log(`packetType: ${packetType}`);
-      console.log(packet);
+      try {
+        switch (packetType) {
+          case PACKET_TYPE.PING:
+            break;
+          case PACKET_TYPE.NORMAL:
+            {
+              const { handlerId, userId, payload } = packetParser(packet);
 
-      switch (packetType) {
-        case PACKET_TYPE.PING:
-          break;
-        case PACKET_TYPE.NORMAL:
-          const { handlerId, payload, userId } = packetParser(packet);
+              const handler = getHandlerById(handlerId);
+              await handler({
+                socket,
+                userId,
+                payload,
+              });
+            }
 
-          console.log('handlerId:', handlerId);
-          console.log('userId:', userId);
-          console.log('payload:', payload);
+            break;
+        }
+      } catch (err) {
+        handleError(socket, err);
       }
     } else {
       // 아직 전체 패킷이 도착 X
