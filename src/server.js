3a20c50c27d811fc860f902f53badf2cf7fb1c1b
SSA <158470963+shinseongan@users.noreply.github.com>
2024-10-24 12:37:24 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ initServer()
       console.log(server.address());
     });
   })
-  .catch((error) => {
-    console.error(error);
+  .catch((err) => {
+    console.error(err);
     process.exit(1); // 오류 발생 시 프로세스 종료
   });
